ba6b30f62662921fabf537c978e095935dac1cfb
Layla Rodas <rodas.layla@gmail.com>
2024-05-11 23:29:53 +0200

    layout component -  logo

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,10 +1,29 @@
 import { Outlet } from "react-router-dom"
+import Logo from "../components/Logo"
 
 export const AppLayout = () => {
   return (
     <>
-        <div>AppLayout</div>
-        <Outlet />
+        <header className="bg-gray-800 py-5">
+            <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                <div className=" w-64">
+                    <Logo />
+                </div>
+
+
+            </div>
+        </header>
+
+        <section className="max-w-screen-2xl mx-auto">
+            <Outlet />
+        </section>
+
+        <footer className="p-5">
+            <p className="text-center">
+                All rights reserved {new Date().getFullYear()}
+            </p>
+        </footer>
+      
     </>
   )
 }
