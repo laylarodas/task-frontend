fff6f18cad82f06885dd2eb8401557bcd8f7d762
Layla Rodas <rodas.layla@gmail.com>
2024-05-11 23:44:05 +0200

    Nav Menu

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 
 export const AppLayout = () => {
   return (
     <>
         <header className="bg-gray-800 py-5">
-            <div className=" max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <div className=" max-w-screen-xl mx-auto flex flex-col lg:flex-row justify-between items-center">
                 <div className=" w-64">
                     <Logo />
                 </div>
 
-
+                <NavMenu />
             </div>
         </header>
 
