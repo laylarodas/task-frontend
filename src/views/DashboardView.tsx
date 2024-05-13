c8e10e8b17ccaa735360cde7ced4afef5feb00d4
Layla Rodas <rodas.layla@gmail.com>
2024-05-13 20:00:18 +0200

    get projects

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,6 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getProjects } from '@/api/ProjectAPI'
 
 export const DashboardView = () => {
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['projects'],
+    queryFn: getProjects
+  })
+
+  if(isLoading) return 'Loading...'
+  console.log(isLoading)
+  console.log(data)
   return (
     <>
       <h1 className=" text-5xl font-black"> My Projects</h1>
