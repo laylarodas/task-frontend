9284f9650bd32ce822d70bdb939e15cd1a8c3403
Layla Rodas <rodas.layla@gmail.com>
2024-05-13 20:13:06 +0200

    validations/zod -- get projects

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,4 +1,4 @@
-import { ProjectFormData } from "@/types/index";
+import { ProjectFormData, dashboardProjectSchema } from "@/types/index";
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
 
@@ -19,7 +19,10 @@ export async function createProject(formData: ProjectFormData) {
 export async function getProjects() {
     try {
         const { data } = await api('/projects')
-        return data
+        const response = dashboardProjectSchema.safeParse(data)
+        if(response.success){
+            return response.data
+        }
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response.data.error)
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -9,10 +9,10 @@ export const DashboardView = () => {
     queryFn: getProjects
   })
 
-  if(isLoading) return 'Loading...'
-  console.log(isLoading)
+  if (isLoading) return 'Loading...'
+
   console.log(data)
-  return (
+  if (data) return (
     <>
       <h1 className=" text-5xl font-black"> My Projects</h1>
       <p className="  text-2xl font-light text-gray-500 mt-5">
@@ -23,6 +23,18 @@ export const DashboardView = () => {
       <nav className='my-5'>
         <Link to='/projects/create' className=' bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white-xl font-bold cursor-pointer transition-colors'>New Project</Link>
       </nav>
+
+      {data.length ? (
+        <p>There are projects</p>
+      ) : (
+        <p className=' text-center py-20'>
+            There are no projects yet {''}
+            <Link 
+                to='/projects/create'
+                className=' text-fuchsia-500 font-bold'
+            >Create Project</Link>
+        </p>
+      )}
     </>
   )
 }
