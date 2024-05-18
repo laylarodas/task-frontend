2fdcd8743ffc8643ce7b9073c298ace981b7be1c
Layla Rodas <rodas.layla@gmail.com>
2024-05-18 10:22:45 +0200

    route and component - project details

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,36 +1,39 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Logo from "@/components/Logo"
 import NavMenu from "@/components/NavMenu"
 
 export const AppLayout = () => {
-  return (
-    <>
-        <header className="bg-gray-800 py-5">
-            <div className=" max-w-screen-xl mx-auto flex flex-col lg:flex-row justify-between items-center">
-                <div className=" w-64">
-                    <Logo />
+    return (
+        <>
+            <header className="bg-gray-800 py-5">
+                <div className=" max-w-screen-xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+                    <div className=" w-64">
+                        <Link to={'/'}>
+                            <Logo />
+                        </Link>
+
+                    </div>
+
+                    <NavMenu />
                 </div>
+            </header>
 
-                <NavMenu />
-            </div>
-        </header>
+            <section className="max-w-screen-xl mx-auto my-10">
+                <Outlet />
+            </section>
 
-        <section className="max-w-screen-xl mx-auto my-10">
-            <Outlet />
-        </section>
+            <footer className="p-5">
+                <p className="text-center">
+                    All rights reserved {new Date().getFullYear()}
+                </p>
+            </footer>
 
-        <footer className="p-5">
-            <p className="text-center">
-                All rights reserved {new Date().getFullYear()}
-            </p>
-        </footer>
-
-        <ToastContainer  
-            pauseOnHover={false}
-            pauseOnFocusLoss={false}
-        />
-    </>
-  )
+            <ToastContainer
+                pauseOnHover={false}
+                pauseOnFocusLoss={false}
+            />
+        </>
+    )
 }
