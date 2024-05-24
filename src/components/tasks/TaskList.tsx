f8dd623a27e0a40928cab59cec23ef18a5acf026
Layla Rodas <rodas.layla@gmail.com>
2024-05-24 18:51:04 +0200

    add status styles

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -19,12 +19,20 @@ const initialStatusGroups: GroupedTasks = {
     completed: []
 }
 
-const statusTranslations: {[key:string]: string} = {
+const statusTranslations: { [key: string]: string } = {
     pending: 'Pending',
     onHold: 'On Hold',
     inProgress: 'In Progress',
     underReview: 'Under Review',
     completed: 'Completed'
+}
+
+const statusStyles: { [key: string]: string } = {
+    pending: 'border-t-slate-500',
+    onHold: 'border-t-red-500',
+    inProgress: 'border-t-blue-500',
+    underReview: 'border-t-amber-500',
+    completed: 'border-t-green-500'
 }
 
 export default function TaskList({ tasks }: TaskListProps) {
@@ -42,8 +50,8 @@ export default function TaskList({ tasks }: TaskListProps) {
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
 
-                        <h3 
-                            className={` capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}
+                        <h3
+                            className={` capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
                         >{statusTranslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
