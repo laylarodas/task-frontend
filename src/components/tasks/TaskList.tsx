4a5cf395842267e163b297d31decf16c87b3f519
Layla Rodas <rodas.layla@gmail.com>
2024-05-24 18:45:05 +0200

    show status tasks

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -19,6 +19,14 @@ const initialStatusGroups: GroupedTasks = {
     completed: []
 }
 
+const statusTranslations: {[key:string]: string} = {
+    pending: 'Pending',
+    onHold: 'On Hold',
+    inProgress: 'In Progress',
+    underReview: 'Under Review',
+    completed: 'Completed'
+}
+
 export default function TaskList({ tasks }: TaskListProps) {
     const groupedTasks = tasks.reduce((acc, task) => {
         let currentGroup = acc[task.status] ? [...acc[task.status]] : [];
@@ -33,6 +41,10 @@ export default function TaskList({ tasks }: TaskListProps) {
             <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
                 {Object.entries(groupedTasks).map(([status, tasks]) => (
                     <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+
+                        <h3 
+                            className={` capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8`}
+                        >{statusTranslations[status]}</h3>
                         <ul className='mt-5 space-y-5'>
                             {tasks.length === 0 ? (
                                 <li className="text-gray-500 text-center pt-3">No tasks</li>
