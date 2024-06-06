7590993bcf41ab70436861cb0ecf49132864536e
Layla Rodas <rodas.layla@gmail.com>
2024-06-06 10:58:15 +0200

    token validation- useMutation

--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -2,22 +2,36 @@ import { Link } from "react-router-dom";
 import { PinInput, PinInputField } from '@chakra-ui/pin-input';
 import { useState } from "react";
 import { ConfirmToken } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { confirmAccount } from "@/api/AuthAPI";
 
 export default function ConfirmAccountView() {
 
     const [ token, setToken ] = useState<ConfirmToken['token']>('')
+
+    const { mutate } = useMutation({
+        mutationFn: confirmAccount,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+    
+    })
 
     const handleChange = (token: ConfirmToken['token']) => {
         setToken(token)
     }
 
     const handleComplete = (token: ConfirmToken['token']) => {
-        console.log('Token:', token)
+        mutate({ token })
     }
 
     return (
         <>
-            <h1 className="text-5xl font-black text-white">Confirm your account</h1>
+            <h1 className="text-5xl font-black text-white text-center">Confirm your account</h1>
             <p className="text-2xl font-light text-white mt-5">
                 Enter the code you received {''}
                 <span className=" text-fuchsia-500 font-bold"> by email </span>
