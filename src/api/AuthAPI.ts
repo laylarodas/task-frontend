217690a8ac5d2b27430911454201a4a1d4b408eb
Layla Rodas <rodas.layla@gmail.com>
2024-06-07 11:46:17 +0200

    confirm code form

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, UserRegistrationForm } from "../types";
+import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -26,3 +26,16 @@ export async function confirmAccount(formData: ConfirmToken) {
         }
     }
 }
+
+export async function RequestConfirmationCode(formData: RequestConfirmationCodeForm) {
+    try {
+        const url = '/auth/request-code'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
