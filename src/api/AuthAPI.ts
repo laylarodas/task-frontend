10235cfb5dcc25f9c8b4e30d1f0c6e9f37219d71
Layla Rodas <rodas.layla@gmail.com>
2024-06-07 11:53:45 +0200

    authenticate user method

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from "../types";
+import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from "../types";
 
 
 export async function createAccount(formData: UserRegistrationForm) {
@@ -30,6 +30,18 @@ export async function confirmAccount(formData: ConfirmToken) {
 export async function RequestConfirmationCode(formData: RequestConfirmationCodeForm) {
     try {
         const url = '/auth/request-code'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+    try {
+        const url = '/auth/login'
         const { data } = await api.post<string>(url, formData)
         return data
     } catch (error) {
