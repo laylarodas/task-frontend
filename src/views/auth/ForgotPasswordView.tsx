6dbdb620e9dec2d76f8ec7c0e4732e52df6cbb3b
Layla Rodas <rodas.layla@gmail.com>
2024-06-08 20:21:19 +0200

    forgot password connect with API

--- src/views/auth/ForgotPasswordView.tsx
+++ src/views/auth/ForgotPasswordView.tsx
@@ -2,6 +2,9 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { ForgotPasswordForm } from "../../types";
 import { ErrorMessage } from "@/components/ErrorMessage";
+import { useMutation } from "@tanstack/react-query";
+import { forgotPassword } from "@/api/AuthAPI";
+import { toast } from "react-toastify";
 
 export default function ForgotPasswordView() {
     const initialValues: ForgotPasswordForm = {
@@ -9,7 +12,18 @@ export default function ForgotPasswordView() {
     }
     const { register, handleSubmit, reset, formState: { errors } } = useForm({ defaultValues: initialValues });
 
-    const handleForgotPassword = (formData: ForgotPasswordForm) => { }
+    const { mutate } = useMutation({
+        mutationFn: forgotPassword,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+            reset()
+        }
+    })
+
+    const handleForgotPassword = (formData: ForgotPasswordForm) => mutate(formData)
 
 
     return (
