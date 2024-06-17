72c6b992a58c97e9c9b146f9518b1b7fc2c9ca2f
Layla Rodas <rodas.layla@gmail.com>
2024-06-17 22:55:39 +0200

    add interceptors - axios

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -18,15 +18,8 @@ export async function createProject(formData: ProjectFormData) {
 
 export async function getProjects() {
 
-    const token = localStorage.getItem('AUTH_TOKEN');
-    console.log(token) 
     try {
-        const { data } = await api('/projects', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        
-        })
+        const { data } = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
         if(response.success){
             return response.data
