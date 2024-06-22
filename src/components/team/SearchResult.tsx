2a072aeaf2929ac2dbfbbbf4951c80c2915c94ed
Layla Rodas <rodas.layla@gmail.com>
2024-06-22 17:34:16 +0200

    add member to project

--- src/components/team/SearchResult.tsx
+++ src/components/team/SearchResult.tsx
@@ -1,16 +1,44 @@
+import { AddUserToProject } from "@/api/TeamAPI"
 import { TeamMember } from "@/types/index"
+import { useMutation } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type SearchResultProps = {
     user: TeamMember
 }
 
 export default function SearchResult({user}: SearchResultProps) {
+
+    const params = useParams()
+    const projectId = params.projectId!
+
+    const { mutate } = useMutation({
+        mutationFn: AddUserToProject,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) => {
+            toast.success(data)
+        }
+
+    })
+
+    const handleAddUserToProject = async () => {
+        const data = {
+            projectId,
+            id: user._id
+        }
+
+        mutate(data)
+    }
+
   return (
     <>
         <p className="mt-10 text-center font-bold">Result</p>
         <div className="flex justify-between items-center">
             <p>{user.name}</p>
-            <button className=" text-purple-600 hover:bg-purple-200 px-10 py-3 font-bold cursor-pointer">Add to Project</button>
+            <button className=" text-purple-600 hover:bg-purple-200 px-10 py-3 font-bold cursor-pointer" onClick={handleAddUserToProject}>Add to Project</button>
         </div>
     </>
   )
