e6923deae3f1b1d08c6a4ff683d0acc74239c335
Layla Rodas <rodas.layla@gmail.com>
2024-07-05 12:32:09 +0200

    user profile form

--- src/types/index.ts
+++ src/types/index.ts
@@ -29,6 +29,7 @@ export const userSchema = authSchema.pick({
 })
 
 export type User = z.infer<typeof userSchema>
+export type UserProfileForm = Pick<User, 'name' | 'email'>
 
 /***   Notes   ***/
 const noteSchema = z.object({
--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,7 +1,14 @@
+import ProfileForm from "@/components/profile/ProfileForm"
+import { useAuth } from "@/hooks/useAuth"
 
 
 export default function ProfileView() {
-  return (
-    <div>ProfileView</div>
-  )
+
+  const {data, isLoading} = useAuth()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  
+  if(data) return <ProfileForm data={data}/>
 }
