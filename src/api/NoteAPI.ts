073611c3e634fe2e424f364281e191ea584e233d
Layla Rodas <rodas.layla@gmail.com>
2024-07-05 07:50:01 +0200

    delete notes

--- src/api/NoteAPI.ts
+++ src/api/NoteAPI.ts
@@ -1,5 +1,5 @@
 import { isAxiosError } from "axios";
-import { NoteFormData, Project, Task } from "../types";
+import { Note, NoteFormData, Project, Task } from "../types";
 import api from "@/lib/axios";
 
 
@@ -7,6 +7,7 @@ type NoteAPIType = {
     formData: NoteFormData
     projectId: Project['_id']
     taskId: Task['_id']
+    noteId: Note['_id']
 }
 
 
@@ -16,6 +17,19 @@ export async function createNote({ formData, projectId, taskId }: Pick<NoteAPITy
     try {
         const url = `/projects/${projectId}/tasks/${taskId}/notes`
         const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response?.data.error)
+        }
+    }
+}
+
+
+export async function deleteNote({ projectId, taskId, noteId }: Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) {
+    try {
+        const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`
+        const { data } = await api.delete<string>(url)
         return data
     } catch (error) {
         if (isAxiosError(error) && error.response) {
