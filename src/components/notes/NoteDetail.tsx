1c4361cfee32227f4a541abf2d1c6506f0c5aa46
Layla Rodas <rodas.layla@gmail.com>
2024-07-05 07:39:28 +0200

    check if user can delete notes

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,5 +1,7 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
 
 
 type NoteDetailProps = {
@@ -7,6 +9,12 @@ type NoteDetailProps = {
 }
 
 export default function NoteDetail({ note }: NoteDetailProps) {
+
+    const { data, isLoading} = useAuth()
+
+    const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+    if(isLoading) return 'Loading...'
     return (
         <div className=" p-3 flex justify-between items-center">
             <div>
@@ -17,6 +25,11 @@ export default function NoteDetail({ note }: NoteDetailProps) {
                     {formatDate(note.createdAt)}
                 </p>
             </div>
+            {canDelete && (
+                <button type="button" className=" bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors">
+                    Delete
+                </button>
+            )}
         </div>
     )
 }
