44748c7e2bfcf4218fe26c3d0062f491e40953f3
Layla Rodas <rodas.layla@gmail.com>
2024-07-09 13:08:52 +0200

    useDraggable - dndContext

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -6,6 +6,8 @@ import { useNavigate, useParams } from "react-router-dom"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { deleteTask } from "@/api/TaskAPI"
 import { toast } from "react-toastify"
+import { useDraggable } from "@dnd-kit/core"
+import { CSS } from '@dnd-kit/utilities';
 
 type TaskCardProps = {
     task: Task,
@@ -13,6 +15,10 @@ type TaskCardProps = {
 }
 
 export default function TaskCard({ task, canEdit }: TaskCardProps) {
+
+    const { attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: task._id
+    })
 
     const navigate = useNavigate();
     const params = useParams()
@@ -32,9 +38,18 @@ export default function TaskCard({ task, canEdit }: TaskCardProps) {
 
     })
 
+    const style = transform ? {
+        transform: CSS.Translate.toString(transform),
+    } :  undefined
+
     return (
         <li className=" p-5 bg-white border border-slate-300 flex justify-between gap-3">
-            <div className=" min-w-0 flex flex-col gap-y-4">
+            <div 
+            {...listeners}
+            {...attributes}
+            ref={setNodeRef}
+            style={style}
+            className=" min-w-0 flex flex-col gap-y-4">
                 <button type="button" className=" text-xl font-bold text-slate-600 text-left" onClick={() => navigate(location.pathname + `?viewTask=${task._id}`)}>{task.name}</button>
                 <p className="text-slate-500">
                     {task.description}
